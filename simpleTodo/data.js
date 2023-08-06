ccad9deeed6ee3e25a3f85c1f1928621b275ce86
estherlardze <estherlardze8@gmail.com>
2023-08-06 13:33:56 +0000

    created todo api in node

--- simpleTodo/data.js
+++ simpleTodo/data.js
@@ -1,30 +1,30 @@
 const todos = [
     {
-      title: "Todo 1",
+      id: 1,
       desc: "This is my first Todo",
       completed: true,
     },
   
     {
-      title: "Todo 2",
+      id:2,
       desc: "This is my second Todo",
       completed: true,
     },
   
     {
-      title: "Todo 3",
+      id: 3,
       desc: "This is my third Todo",
       completed: true,
     },
   
     {
-      title: "Todo 4",
+      id: 4,
       desc: "This is my fourth Todo",
       completed: true,
     },
   
     {
-      title: "Todo 5",
+      id: 5,
       desc: "This is my fifth Todo",
       completed: true,
     },
--- simpleTodo/index.js
+++ simpleTodo/index.js
@@ -1,19 +1,56 @@
-const http = require("http")
-const fs = require("fs")
+const express = require("express")
+const app = express()
 const todos = require("./data")
 const port = 3000
 
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type" : "application/json"});
-    const pathUrl = req.url;
-   
-    switch(pathUrl){
-        case "/todo":
-         res.end(JSON.stringify(todos))
-         break;
-    }
-  
+//get all todos on port 3000 
+app.get("/", (req, res) => {
+    res.status(200).json(todos)
 })
 
-server.listen(port, console.log(`server is running on port ${port}`))
+//get all todos on port 3000 '/api/todos'
+app.get('/api/todos', (req, res) => {
+    res.status(200).json(todos)
+})
+
+// get a specific todo
+app.get('/api/todos/:id', (req, res) => {
+    const todo = todos.find((item )=> item.id === parseInt(req.params.id))
+    if(todo){
+      res.status(200).json(todo)
+    }else{
+        res.status(404).json({massage: "Todo not found"})
+    }
+})
+    
+// post a todo
+app.post('/api/todos', (req, res) => {
+    const newTodo = req.body;   
+    todos.push(newTodo)
+    res.status(201).json(newTodo)
+
+})
+
+// update a todo
+app.put('/api/todos/:id', (req, res) => {
+    const todo = todos.find((item )=> item.id === parseInt(req.params.id))
+
+    if(todo){
+        const {id, desc, completed } = req.body;
+        todo.id = id
+        todo.desc = desc
+        todo.completed = completed
+        res.status(200).json({massage: 'Todo updated successfully'})       
+    }else{
+        res.status(404).json({massage: 'Todo unable to update'}) 
+    }
+})
+
+//delete a todo
+app.delete('/api/todos/:id', (req, res) => {
+    const todoId = parseInt(req.params.id)
+    const deletedTodo = todos.filter((todo) => todo.id !== todoId)
+    res.json(deletedTodo)
+})
+
+app.listen(port, console.log(`server is running on port ${port}`))
